3a1e61cc7b0b249a9f2e3fce9cfa6cf6b766cb2a
Talley Lambert <talley.lambert@gmail.com>
2022-06-27 22:20:52 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,5 @@
+# only needed until setuptools implements PEP 660
+# https://github.com/pypa/setuptools/issues/2816
+import setuptools
+
+setuptools.setup()
---
+++ src/app_model/__init__.py
@@ -0,0 +1,10 @@
+"""Generic application schema implemented in python"""
+
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("app-model")
+except PackageNotFoundError:
+    __version__ = "uninstalled"
+__author__ = "Talley Lambert"
+__email__ = "talley.lambert@gmail.com"
