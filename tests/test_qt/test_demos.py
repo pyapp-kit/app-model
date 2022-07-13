052016f78531adac9db0fd98a39ea1b9c69c0cf7
Talley Lambert <talley.lambert@gmail.com>
2022-07-12 21:53:16 -0400

    test: add fixture to test qapp

--- tests/test_qt/test_demos.py
+++ tests/test_qt/test_demos.py
@@ -8,6 +8,6 @@ DEMO = Path(__file__).parent.parent.parent / "demo"
 
 
 @pytest.mark.parametrize("fname", ["qapplication.py", "model_app.py"])
-def test_qapp(fname, monkeypatch):
+def test_qapp(qapp, fname, monkeypatch):
     monkeypatch.setattr(QApplication, "exec_", lambda *a, **k: None)
     runpy.run_path(str(DEMO / fname), run_name="__main__")
