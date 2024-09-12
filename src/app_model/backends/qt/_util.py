934c56aa9d92824918959ed13941256112a8e7d8
Daniel Althviz Moré <16781833+dalthviz@users.noreply.github.com>
2024-09-12 10:15:45 -0500

    feat: add support for `.svg` file paths as `Action` icon (#219)
    * feat: add support for '.svg' file paths as action icons
    
    * Add .svg file as icon usage over model_app demo code
    
    * use pathlib path
    
    * change conditional
    
    * Update demo/model_app.py
    
    * Add file URI scheme usage possibility and Windows note to icon field description
    
    ---------
    
    Co-authored-by: Talley Lambert <talley.lambert@gmail.com>

--- src/app_model/backends/qt/_util.py
+++ src/app_model/backends/qt/_util.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+from qtpy.QtCore import QUrl
 from qtpy.QtGui import QIcon
 
 if TYPE_CHECKING:
@@ -15,7 +16,9 @@ def to_qicon(icon: Icon, theme: Literal["dark", "light"] = "dark") -> QIcon:
     from superqt import QIconifyIcon, fonticon
 
     if icn := getattr(icon, theme, ""):
-        if ":" in icn:
+        if icn.startswith("file://"):
+            return QIcon(QUrl(icn).toLocalFile())
+        elif ":" in icn:
             return QIconifyIcon(icn)
         else:
             return fonticon.icon(icn)
