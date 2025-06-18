a95379ff38b207cce6cf9a3ebd3afe782f23790b
Talley Lambert <talley.lambert@gmail.com>
2025-06-18 16:06:38 -0400

    fix: order of menu registration to come after keybindings registration (#249)
    * fix: reorder menu registration to ensure it occurs after command registration
    
    * fix test
    
    * change to shift

--- tests/test_qt/test_qmainwindow.py
+++ tests/test_qt/test_qmainwindow.py
@@ -1,14 +1,22 @@
+from __future__ import annotations
+
 from typing import TYPE_CHECKING
 
 from qtpy.QtCore import Qt
+from qtpy.QtWidgets import QAction, QApplication
 
 from app_model.backends.qt import QModelMainWindow, QModelToolBar
+from app_model.types._action import Action
 
 if TYPE_CHECKING:
+    from pytestqt.qtbot import QtBot
+
     from ..conftest import FullApp  # noqa: TID252
 
 
-def test_qmodel_main_window(qtbot, full_app: "FullApp") -> None:
+def test_qmodel_main_window(
+    qtbot: QtBot, qapp: QApplication, full_app: FullApp
+) -> None:
     win = QModelMainWindow(full_app)
     qtbot.addWidget(win)
 
@@ -27,3 +35,16 @@ def test_qmodel_main_window(qtbot, full_app: "FullApp") -> None:
     )
     assert isinstance(tb, QModelToolBar)
     win.addModelToolBar(full_app.Menus.EDIT, area=Qt.ToolBarArea.RightToolBarArea)
+
+    full_app.register_action(
+        Action(
+            id="late-action",
+            title="Late Action",
+            keybindings=[{"primary": "Shift+L"}],
+            menus=[{"id": full_app.Menus.FILE}],
+            callback=lambda: None,
+        )
+    )
+
+    action = qapp.findChild(QAction, "late-action")
+    assert action.shortcut().toString() == "Shift+L"
