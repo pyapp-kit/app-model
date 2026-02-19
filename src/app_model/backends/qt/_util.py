d418f7090239e03238bcbd92f2949cebb5452dd5
Lorenzo Gaifas <brisvag@gmail.com>
2026-02-19 18:30:54 +0100

    add simple color guessing

--- src/app_model/backends/qt/_util.py
+++ src/app_model/backends/qt/_util.py
@@ -15,11 +15,12 @@ def to_qicon(icon: Icon, theme: Literal["dark", "light"] = "dark") -> QIcon:
     """Create QIcon from Icon."""
     from superqt import QIconifyIcon, fonticon
 
+    color = 'white' if theme == 'dark' else 'black'
     if icn := getattr(icon, theme, ""):
         if icn.startswith("file://"):
             return QIcon(QUrl(icn).toLocalFile())
         elif ":" in icn:
-            return QIconifyIcon(icn)
+            return QIconifyIcon(icn, color=color)
         else:
-            return fonticon.icon(icn)
+            return fonticon.icon(icn, color=color)
     return QIcon()  # pragma: no cover
