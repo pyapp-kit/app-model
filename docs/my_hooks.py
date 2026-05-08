2db346928c818e44006d70f5eef594586c25c134
Talley Lambert <talley.lambert@gmail.com>
2026-05-08 15:12:06 -0400

    docs: use zensical

--- docs/my_hooks.py
+++
@@ -1,7 +0,0 @@
-from typing import Any
-
-
-def on_page_markdown(md: str, **kwargs: Any) -> str:
-    T = "::: app_model.types"
-    T2 = T + "\n\toptions:\n\t\tdocstring_section_style: table"
-    return md.replace(T, T2)
